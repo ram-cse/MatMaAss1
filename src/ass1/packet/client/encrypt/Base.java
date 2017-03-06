6ea8a9ffac92d26f00bb71726e39f2818b686311
RamDieu <51303392@hcmut.edu.vn>
2017-03-06 16:23:13 +0700

    Demo

--- src/ass1/packet/client/encrypt/Base.java
+++
@@ -1,7 +0,0 @@
-package ass1.packet.client.encrypt;
-
-public interface Base {
-	public void encryt();
-
-	public void decryt();
-}
---
+++ src/ass1/packet/encryption/RSA.java
@@ -0,0 +1,23 @@
+package ass1.packet.encryption;
+
+public class RSA extends Cryptor{
+
+	@Override
+	public byte[] ecrypt(byte[] plain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] decrypt(byte[] cypher) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void generateKey() {
+		// TODO Auto-generated method stub
+		
+	}	
+	
+}
